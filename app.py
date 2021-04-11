8b1f7effaa64e3a06cd84a247dcbf1556cd6b375
skota007 <skota3121@gmail.com>
2021-04-12 00:26:21 +0300

    procfile

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+import app
 
 app = Flask(__name__)
+server = flask.Flask(__name__)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
 
 @app.route("/")
 def index():
@@ -9,6 +12,6 @@ def index():
 @app.route("/doua")
 def joc():
     return render_template("joc.html")
-import app
+
 if __name__ == '__main__':
     app.run()
